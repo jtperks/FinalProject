9f909aff634aa3fe06944ba2c7f6e0ec46ab9d85
Joel Perkins <32662940+jtperks@users.noreply.github.com>
2022-11-23 11:43:33 -0800

    Add project files.

---
+++ Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    internal class Inventory
+    {
+        private string itemNum;
+        private string desc;
+        private double pricePerItem;
+        private int quantity;
+        private double ourCost;
+        private double totalValue;
+       
+        public string ItemNum
+        {
+            get { return itemNum; }
+            set { itemNum = value; }
+        }
+        public string getItemNum()
+        {
+            if (ItemNum == null)
+            {
+                return "That item doesn't exist.";
+            }
+            else
+            {
+                return ItemNum;
+            }
+
+
+        }
+
+        public void setItemNum()
+        {
+            Console.Write("What is the item #? ");
+            itemNum = Console.ReadLine();
+            
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory peoples = new Inventory();
+            //Console.WriteLine(peoples.ItemNum);
+            //peoples.ItemNum = "405064";
+            string respo = peoples.getItemNum();
+            Console.WriteLine(respo);
+            //Console.WriteLine(peoples.ItemNum);
+            Console.ReadLine();
+            
+        }
+    }
+}
